b7b32c35390899dcf1c1e768b82c4c9b34d04860
alkove <alexei.kovelman@gmail.com>
2018-06-05 13:32:24 +0300

    Implement serial port polling and diff formatting

--- Console/Program.cs
+++ Console/Program.cs
@@ -3,13 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Management;
+using shared;
 
-namespace Console
+namespace TestConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
+            IOrderedEnumerable<string> older_ports = Serial.GetSortedSerialPortNames();
+            IOrderedEnumerable<string> newer_ports = older_ports;
+
+            while (true)
+            {
+                System.Threading.Thread.Sleep(1000);
+
+                newer_ports = Serial.GetSortedSerialPortNames();
+
+                if (!Enumerable.SequenceEqual(older_ports, newer_ports))
+                {
+                    Console.Write(Serial.GetDiff(older_ports, newer_ports));
+                }
+
+                older_ports = newer_ports;
+            }
         }
     }
 }
---
+++ shared/serial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace shared
+{
+    public class Serial
+    {
+        public static IOrderedEnumerable<string> GetSortedSerialPortNames()
+        {
+            return SerialPort.GetPortNames().OrderBy(t => t); // returns COM1, COM5, etc.
+            
+        }
+
+        public static string GetDiff(IOrderedEnumerable<string> old, IOrderedEnumerable<string> newer)
+        {
+            StringBuilder sb = new StringBuilder();
+             foreach (string s in old.Except(newer))
+            {
+                // removed devices
+                sb.AppendFormat(" - {0}\n", s);
+            }
+
+             foreach (string s in newer.Except(old))
+            {
+                // new devices
+                sb.AppendFormat(" + {0}\n", s);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
